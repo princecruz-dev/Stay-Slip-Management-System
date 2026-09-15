4ff442e3d1175b678aca5639169558e4eba17c58
Xian-1026 <ronaldhalili05@gmail.com>
2026-09-15 15:04:18 +0800

    done

--- app/src/test/java/quarter2/Delgado.java
+++ app/src/test/java/quarter2/Delgado.java
@@ -1,4 +1,20 @@
 package quarter2;
-
+import org.junit.Test;
 public class Delgado {
+
+   @Test
+    public void printMYprofile(){
+        String myName = "Xian";
+        String petname1 = "Oyo";
+        String petname2 = "Ruth";
+        String favfood = "Lechon";
+        int myAge = 17;
+
+        System.out.println("--- MY DIGITAL PROFILE ---");
+        System.out.println("Hello, my name is " + myName + " and I am + myAge +");
+        System.out.println("I have 2 wonderful pets named" + petname1 + " and " + petname2 + ".");
+        System.out.println("If I could eat " + favfood + " everyday!");
+
+   }
+
 }
