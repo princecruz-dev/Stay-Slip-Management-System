8ccdc6f1f2a85cf006a64053037c25eb1c7d0650
neil-sourcebuddy <neilmarshallbmaingat@gmail.com>
2026-09-15 14:24:19 +0800

    Neil Maingat Mini_peta_2

---
+++ app/src/test/java/quarter2/MaingatProfileTest.java
@@ -0,0 +1,15 @@
+package quarter2;
+import org.junit.Test;
+public class MaingatProfileTest {
+    @Test
+    public void MyProfile() {
+        String myName = "Neil";
+        String favFood = "French Fries";
+        String favPlace = "Binondo";
+        int myAge = 16;
+
+        System.out.println("--- MY PROFILEEEE ---");
+        System.out.println("Hello! my Name is " + myName + ", and I am " + myAge + " years old.");
+        System.out.println("My favorite food is " + favFood + " and my favorite place to go to is " + favPlace +"!");
+    }
+}
