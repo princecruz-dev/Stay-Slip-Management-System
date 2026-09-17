1461a0e7e27b92a43643508dec58e5f2aff12740
princecruz-dev <princeadriancruz6@gmail.com>
2026-09-17 22:05:14 +0800

    CRUZ Commit for Mini Peta 2

---
+++ app/src/test/java/com/example/stay_slip_management_system/quarter2/CruzProfileTest.java
@@ -0,0 +1,17 @@
+package com.example.stay_slip_management_system.quarter2;
+
+import org.junit.Test;
+
+public class CruzProfileTest {
+    @Test
+    public void MyProfile() {
+        String myName = "Prince";
+        String favPlace = "Intramuros";
+        String favFood = "Adobo";
+        int myAge = 17;
+
+        System.out.println("--- MY PROFILEEEE ---");
+        System.out.println("Hello! my Name is " + myName + ", and I am " + myAge + " years old.");
+        System.out.println("My favorite food is " + favFood + " and my favorite place to go to is " + favPlace + "!");
+    }
+}
